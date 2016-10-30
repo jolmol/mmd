fd4fa718ce80d31ac7ccfa793afca25deddc2b14
mohamad007 <mohamad137977@gmail.com>
2016-10-30 14:32:50 +0330

    Add files via upload

---
+++ dati.php
@@ -0,0 +1,12 @@
+<?php
+
+//APIKEY
+$apiKey = "197395019:AAG4aXj9MQt2Zb-Hq3dazXVmeRIyKaA2_BY";
+//TUOID
+$inserisciIlTuoId = "193930120";
+//puoi trovare il tuo id mandando un messaggio
+//a @userinfobot
+
+//BOT CREATO DA @xMicky24YT PER QUALSIASI PROBLEMA NON ESITARE A CONTATTARMI
+
+?>
